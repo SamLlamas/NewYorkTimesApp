bad4b9000be0172230bd10763a7978e808c681f4
ektravel <38017980+ektravel@users.noreply.github.com>
2018-04-27 10:59:19 -0500

    committing latest changes

--- app.js
+++ app.js
@@ -3,38 +3,65 @@ var seachTerm = "";
 var startDate = "" + "0101";
 var endDate = "" + "0101";
 var numberOfRecords = 0;
+var limit = 10;
 
-function searchNYTimes(seachTerm) {
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "117bb279ebae41db9ebdf0268997281b",
-  'q': searchTerm,
-  'begin_date': startDate,
-  'end_date': endDate
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
-$("#searchButton").on("click", function(){
-    searchTerm = $("#searchParam").val().trim();
-    startDate = $("#inputStart"+"0101").val().trim();
-    endDate = $("#inputEnd"+"0101").val().trim();
-    $("#SelectEntityPrimaryName option:selected").val()
 
+$("#searchButton").on("click", function (event) {
+  event.preventDefault();
+  console.log("clicked");
+  searchTerm = $("#inputTerm").val().trim();
+  startDate = $("#inputStart").val().trim() + "0101";
+  endDate = $("#inputEnd").val().trim() + "0101";
+  console.log("limit: " + limit);
+  limit = $("#numbers").val().trim();
+
+
+  console.log("searchTerm: " + searchTerm);
+  console.log("startDate: " + startDate);
+  console.log("endDate: " + endDate);
+  console.log("limit: " + limit);
+
+
+  $("#SelectEntityPrimaryName option:selected").val()
+
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': "117bb279ebae41db9ebdf0268997281b",
+    'q': searchTerm,
+    'begin_date': startDate,
+    'end_date': endDate
+  });
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function (result) {
+    console.log(result);
+    //$("#articlesContainer").append("<p>" + result.response.docs[0].byline.original + "</p>");
+
+    for (var i = 0; i < limit; i++) {
+      $("#articlesContainer").append("<p>" + result.response.docs[i].byline.original + "</p>");
+      console.log(result.response.docs[i].byline.original);
+
+    }
+
+
+
+
+
+
+
+  }).fail(function (err) {
+    throw err;
+  });
 });
+
 var numberOfRecords = $("#inputNum").val();
 
-//create a for loop and limit the number of indexes
-//number of articles = set value $(""). 
-//response.docs for loop
-for (i = 0; i <= numberOfRecords; i++) {
+  //create a for loop and limit the number of indexes
+  //number of articles = set value $(""). 
+  //response.docs for loop
+  // for (i = 0; i <= numberOfRecords; i++) {
 
 
-};
-};
+  // };
